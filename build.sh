e6d3c8db51752179e630ab271eba4eea53dd2ac2
jahway603 <jahway603@protonmail.com>
2021-07-08 12:14:09 -0400

    added rustc and cargo checks to build.sh

--- build.sh
+++ build.sh
@@ -13,6 +13,18 @@ else
     JOBS=1
 fi
 
+# check if rustc and cargo are installed, otherwise exit with error
+if ! command -v rustc &> /dev/null
+then
+    echo "rustc could not be found. Please install it and try again."
+    exit
+fi
+if ! command -v cargo &> /dev/null
+then
+    echo "cargo could not be found. Please install it and try again."
+    exit
+fi
+
 VERSION=$(cat src/version.h |cut -d\" -f2)
 echo "Compiling SilentDragonLite $VERSION with $JOBS threads..."
 CONF=silentdragon-lite.pro
