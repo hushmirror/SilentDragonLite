ca0f770fb537f07dd34b590f959f889ccbb22954
onryo <onryo@hush.community>
2021-10-18 22:08:23 +0000

    Upload files to ''

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright 2019-2021 The Hush Developers
+
+./build.sh linguist && ./build.sh
+
+username=$(id -un)
+
+sed -i "s|\/home\/.*\/SilentDragonLite\/|\/home\/$username\/SilentDragonLite\/|g" SilentDragonLite.desktop
+
+mv SilentDragonLite.desktop ~/.local/share/applications
