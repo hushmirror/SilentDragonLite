c14c7f2b4b44856cb83c54248731d86a35d1a2c3
Duke Leto <duke@leto.net>
2021-12-01 13:52:06 -0500

    add translation analysis script

---
+++ src/scripts/translation_analysis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -n AR: ;grep unfinished silentdragonlite_ar.ts | wc -l  
+echo -n BE: ;grep unfinished silentdragonlite_be.ts | wc -l
+echo -n DE: ;grep unfinished silentdragonlite_de.ts | wc -l
+echo -n ES: ;grep unfinished silentdragonlite_es.ts | wc -l
+echo -n FA: ;grep unfinished silentdragonlite_fa.ts | wc -l
+echo -n FR: ;grep unfinished silentdragonlite_fr.ts | wc -l
+echo -n HR: ;grep unfinished silentdragonlite_hr.ts | wc -l
+echo -n ID: ;grep unfinished silentdragonlite_id.ts | wc -l
+echo -n IT: ;grep unfinished silentdragonlite_it.ts | wc -l
+echo -n NL: ;grep unfinished silentdragonlite_nl.ts | wc -l
+echo -n PT: ;grep unfinished silentdragonlite_pt.ts | wc -l
+echo -n RO: ;grep unfinished silentdragonlite_ro.ts | wc -l
+echo -n RU: ;grep unfinished silentdragonlite_ru.ts | wc -l
+echo -n SR: ;grep unfinished silentdragonlite_sr.ts | wc -l
+echo -n TR: ;grep unfinished silentdragonlite_tr.ts | wc -l
+echo -n UD: ;grep unfinished silentdragonlite_ud.ts | wc -l
+echo -n ZH: ;grep unfinished silentdragonlite_zh.ts | wc -l
